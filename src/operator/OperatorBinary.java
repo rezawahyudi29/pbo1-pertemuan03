02d77e00564b5f04cafb3321635f7e96b24e3d39
rezawahyudi29 <rezawhydi29@gmail.com>
2020-04-28 06:01:33 -0700

    Operator Unary

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class OperatorBinary {
+
+    String nama = "M.FAHREZA WAHYUDI";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+}
